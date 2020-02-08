4e42eb3149228d858e128f08f7d227b4d02a1aff
rosario VS <paradisekiss-yaoi@hotmail.com>
2020-02-08 14:00:28 -0600

    Fetch Read Files

---
+++ ajaxReadFiles/script.js
@@ -0,0 +1,24 @@
+/*
+    .blob(): Binary Long Object
+
+*/
+
+const buttonImg = document.getElementById('button-img')
+const buttonPDF = document.getElementById('button-pdf')
+
+buttonImg.addEventListener('click', ()=> {
+    fetch('tkm.jpg')
+        .then(res => res.blob())
+        .then(img => {
+            document.getElementById('img').src = URL.createObjectURL(img)
+        })
+
+})
+
+buttonPDF.addEventListener('click', ()=> {
+    fetch('unidad1.pdf')
+        .then(res => res.blob())
+        .then(pdf => {
+            document.getElementById('pdf').href = URL.createObjectURL(pdf)
+        })
+})
